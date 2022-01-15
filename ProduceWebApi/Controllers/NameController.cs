861ca3a420212a997ac284c34d1f6aa0a5c2372f
Gareth Baumgart <gareth.baumgart@wexinc.com>
2022-01-15 16:24:43 +1100

    Add initial projects and container

---
+++ ProduceWebApi/Controllers/NameController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProduceWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> AddName(string name)
+        {
+            return Ok();
+        }
+    }
+}
